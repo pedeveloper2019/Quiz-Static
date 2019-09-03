ddfd8386070826c58d7c9dfd5149b5eee2dcb2b3
pedeveloper2019 <pedeveloper.2019@gmail.com>
2019-09-03 17:46:52 +0530

    .

--- assets/js/sideHide.js
+++ assets/js/sideHide.js
@@ -2,7 +2,7 @@ $(function () {
     $(".hider").click(function (e) { 
         e.preventDefault();
         $(".Qselect").toggle();
-        $(".QWrapper").toggleClass("col-md-12");
+        $(".QWrapper").toggleClass("col-xl-12");
         $(".QWrapper .question img").toggleClass("w-50");
     });
     
